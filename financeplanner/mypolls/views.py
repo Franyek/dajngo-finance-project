7711c8c3b206c66372630830d54a158d37326a5b
Franyek <franyek@gmail.com>
2019-10-01 11:38:33 +0200

    feature(django generic views)

--- financeplanner/mypolls/views.py
+++ financeplanner/mypolls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 # Create your views here.
 from django.template import loader
@@ -8,14 +9,22 @@ from django.urls import reverse
 from .models import Question, Choice
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/details.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {"question": question})
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
+
+
+class ResultView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
 
 
 def vote(request, question_id):
@@ -25,7 +34,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(
             request,
-            'polls/details.html',
+            'polls/detail.html',
             {
                 'question': question,
                 'error_message': "You didnt select a choice"
@@ -35,11 +44,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
