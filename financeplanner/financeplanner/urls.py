497e7df5def85b1143256556b6faf967fe15c5a5
Franyek <franyek@gmail.com>
2019-10-03 10:30:13 +0200

    refact(rename mypolls app)

--- financeplanner/financeplanner/urls.py
+++ financeplanner/financeplanner/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('mypolls.urls')),
+    path('polls/', include('polls.urls')),
     path('about/', include('about.urls'))
 ]
--- financeplanner/mypolls/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MypollsConfig(AppConfig):
-    name = 'mypolls'
