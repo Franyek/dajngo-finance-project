69190e0e528f425429ad0c8815a850cd7e9be426
Franyek <franyek@gmail.com>
2019-10-04 13:15:10 +0200

    feature(blog index)

--- financeplanner/blog/models.py
+++ financeplanner/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField("date published")
+    title = models.CharField(max_length=100)
+
+    def get_first_paragraph(self):
+        return self.text.splitlines()[0]
+
+    def __str__(self):
+        return self.title
--- financeplanner/blog/views.py
+++ financeplanner/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
+from .models import BlogPost
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = "blog/index.html"
+    context_object_name = "latest_blog_posts"
+
+    def get_queryset(self):
+        return BlogPost.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
